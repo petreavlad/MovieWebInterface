e4d91731603f134d2f4904ed05df45633b3ea032
Petrea Vlad <petreavlad26@gmail.com>
2021-01-02 22:59:23 +0200

    Api doc page changes

--- src/components/ApiDocumentationPage.js
+++ src/components/ApiDocumentationPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./ApiDocumentationPage.css";
 import page from "../utils/PageDocumentation.json";
+import ReactJson from "react-json-view";
 
 function ApiDocumentationPage(props) {
   const [divArray, setDivArray] = useState([]);
@@ -18,7 +19,7 @@ function ApiDocumentationPage(props) {
       <div id="page_item_holder">
         <div id="page_item_title">
           <b>
-            <u>Registration</u>
+            <u>{item.name}</u>
           </b>
         </div>
         <div id="page_header_holder">
@@ -28,6 +29,103 @@ function ApiDocumentationPage(props) {
           <div id="page_item_url">
             <b>{item.url}</b>
           </div>
+        </div>
+        {[getParts(item)]}
+      </div>
+    );
+  }
+
+  function getParts(item) {
+    let array = [];
+    if (item.headers) {
+      array.push(getheaders(item));
+    }
+    if (item.body) {
+      array.push(getBodyExample(item));
+    }
+    if (item.response) {
+      array.push(getResponseExample(item));
+    }
+
+    array.push(getBottomSpace());
+
+    return array;
+  }
+
+  function getheaders(item) {
+    return (
+      <div id="page_item_body_holder">
+        <div id="page_item_body_title">
+          <b>
+            <u>Headers</u>
+          </b>
+        </div>
+        <div id="page_item_body_example_holder">
+          <ReactJson
+            src={item.headers}
+            theme="ocean"
+            displayObjectSize="true"
+            displayDataTypes={false}
+            displayObjectSize={false}
+            name={false}
+            style={{ backgroundColor: "#e23e57" }}
+          ></ReactJson>
+        </div>
+      </div>
+    );
+  }
+
+  function getBodyExample(item) {
+    let bodyExample;
+    if (item.body_type) {
+      bodyExample = "Body example" + " (" + item.body_type + ")";
+    } else {
+      bodyExample = "Body example";
+    }
+    return (
+      <div id="page_item_body_holder">
+        <div id="page_item_body_title">
+          <b>
+            <u>{bodyExample}</u>
+          </b>
+        </div>
+        <div id="page_item_body_example_holder">
+          <ReactJson
+            src={item.body}
+            theme="ocean"
+            displayObjectSize="true"
+            displayDataTypes={false}
+            displayObjectSize={false}
+            name={false}
+            style={{ backgroundColor: "#e23e57" }}
+          ></ReactJson>
+        </div>
+      </div>
+    );
+  }
+
+  function getBottomSpace() {
+    return <div id="page_item_bottom_margin"></div>;
+  }
+
+  function getResponseExample(item) {
+    return (
+      <div id="page_item_body_holder">
+        <div id="page_item_body_title">
+          <b>
+            <u>Response structure</u>
+          </b>
+        </div>
+        <div id="page_item_body_example_holder">
+          <ReactJson
+            src={item.response}
+            theme="ocean"
+            displayObjectSize="true"
+            displayDataTypes={false}
+            displayObjectSize={false}
+            name={false}
+            style={{ backgroundColor: "#e23e57" }}
+          ></ReactJson>
         </div>
       </div>
     );
